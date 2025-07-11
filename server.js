c5b9b16c4f7cca74471371a9e356f5c9284c76d3
Rohityadav1256 <20230802080@dypiu.ac.in>
2025-07-11 18:01:19 +0530

    Add files via upload
    REAL-TIME COLLABORATION
    TOOL

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('textChange', (data) => {
+    socket.broadcast.emit('textUpdate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
